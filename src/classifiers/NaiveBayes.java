59590e69c3b4c46004b4b6aa48b145b5282329ed
SaadullahKarimi <79996938+SaadullahKarimi@users.noreply.github.com>
2021-05-20 11:24:50 +0430

    Update NaiveBayes.java

--- src/classifiers/NaiveBayes.java
+++ src/classifiers/NaiveBayes.java
@@ -1,19 +1,4 @@
-/* 
- * Copyright (C) 2014 Vasilis Vryniotis <bbriniotis at datumbox.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+
 package com.datumbox.opensource.classifiers;
 
 import com.datumbox.opensource.dataobjects.Document;
@@ -29,10 +14,6 @@ import java.util.Map;
 
 /**
  * Implements a basic form of Multinomial Naive Bayes Text Classifier as described at
- * http://blog.datumbox.com/machine-learning-tutorial-the-naive-bayes-text-classifier/
- * 
- * @author Vasilis Vryniotis <bbriniotis at datumbox.com>
- * @see <a href="http://blog.datumbox.com/developing-a-naive-bayes-text-classifier-in-java/">http://blog.datumbox.com/developing-a-naive-bayes-text-classifier-in-java/</a>
  */
 public class NaiveBayes {
     private double chisquareCriticalValue = 10.83; //equivalent to pvalue 0.001. It is used by feature selection algorithm
